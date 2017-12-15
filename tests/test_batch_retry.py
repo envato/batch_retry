fbca4c00a267bb8c80a317ae5843f97c8a55ef51
Patrick Robinson <patrick.robinson@envato.com>
2017-12-15 14:51:18 +1100

    Create a failing test

--- tests/test_batch_retry.py
+++ tests/test_batch_retry.py
@@ -11,8 +11,6 @@ class TestKinesisProcessorWithRetries(unittest.TestCase):
         self.event_2 = {"SomeEvent2": "text2"}
         self.put_records_attempts = 0
 
-        patch('time.sleep').start()
-
     def tearDown(self):
         patch.stopall()
 
@@ -47,9 +45,18 @@ class TestKinesisProcessorWithRetries(unittest.TestCase):
             'EncryptionType': 'NONE'
         }
 
-    def test_puts_events_with_retries(self):
+    @patch('time.sleep')
+    def test_puts_events_with_retries(self, MockSleep):
         kinesis_client_mock = MagicMock()
         kinesis_client_mock.put_records.side_effect = self.mock_put_records_fail_first_attempt
         send_function = KinesisProcessor(kinesis_client_mock, self.stream_name, batch_size=2).send
         BatchRetry(send_function).send_with_retries([self.event_1, self.event_2])
         self.assertEqual(2, self.put_records_attempts)
+
+    @patch('time.sleep')
+    def test_sleeps_for_milliseconds(self, MockSleep):
+        kinesis_client_mock = MagicMock()
+        kinesis_client_mock.put_records.side_effect = self.mock_put_records_fail_first_attempt
+        send_function = KinesisProcessor(kinesis_client_mock, self.stream_name, batch_size=2).send
+        BatchRetry(send_function).send_with_retries([self.event_1, self.event_2])
+        MockSleep.assert_called_once_with(0.2)
